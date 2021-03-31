c46ef832d5b29ae51d96e02ae46b4fed1dcc7f1e
rin1782 <colaerin@yahoo.com>
2021-03-30 23:22:52 -0400

    created 3 fetch requests to the backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :pets
   resources :users
+  delete "/pets/:id", to: "pets#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
